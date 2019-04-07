4e98efce9a4001c0bd119b4241acdb123965427c
Naveen Adarsh Petla <npetla@vmware.com>
2019-04-07 11:32:12 +0530

    Correction to binary search

--- Binary Search/binearySearch.js
+++ Binary Search/binearySearch.js
@@ -14,7 +14,7 @@ function BinarySearchArrayElement(arr, el) {
             } else if (el > arr[mid]) {
                 start = mid + 1;
             } else {
-                end = mid;
+                end = mid - 1;
             }
         }
     }
@@ -25,6 +25,7 @@ const testArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 console.log(1, BinarySearchArrayElement(testArr1, 1));
 console.log(4, BinarySearchArrayElement(testArr1, 4));
+console.log(0, BinarySearchArrayElement(testArr1, 0));
 console.log(10, BinarySearchArrayElement(testArr1, 10));
 console.log(6, BinarySearchArrayElement(testArr1, 6));
 console.log(9, BinarySearchArrayElement(testArr1, 9));
