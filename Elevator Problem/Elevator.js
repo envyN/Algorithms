10ae33b603f35e6cc9468baf11648492b9687a39
Naveen Adarsh Petla <npetla@vmware.com>
2019-04-07 11:36:45 +0530

    Formatted in webstorm

--- Elevator Problem/Elevator.js
+++ Elevator Problem/Elevator.js
@@ -1,181 +1,188 @@
 window.onload = function () {
-	var submitButton = document.getElementById('submit'),
-	submitPass = document.getElementById('submitPass'),
-	result = document.getElementById('result'),
-	pass,
-	stops;
+    var submitButton = document.getElementById('submit'),
+        submitPass   = document.getElementById('submitPass'),
+        result       = document.getElementById('result'),
+        pass,
+        stops;
 
-	submitPass.onclick = function () {
-		pass = document.getElementById('passengers').value;
-		if (isNaN(pass) || pass < 1) {
-			result.innerHTML = '<red>Zero Passengers!!! Enter a number > 0</red>';
-			submitButton.disabled = true;
-			return;
-		} else {
-			conDiv = document.getElementById('condiv');
-			conDiv.innerHTML = '';
-			for (var i = 0; i < pass; i++) {
-				var p = '<br />Passenger ' + (i + 1) + '\'s destination :<input id="p' + i + '" type="text" />';
-				conDiv.innerHTML += p;
-			}
-		}
-		submitButton.disabled = false;
-	}
+    submitPass.onclick = function () {
+        pass = document.getElementById('passengers').value;
+        if (isNaN(pass) || pass < 1) {
+            result.innerHTML      = '<red>Zero Passengers!!! Enter a number > 0</red>';
+            submitButton.disabled = true;
+            return;
+        } else {
+            conDiv           = document.getElementById('condiv');
+            conDiv.innerHTML = '';
+            for (var i = 0; i < pass; i++) {
+                var p = '<br />Passenger ' + (i + 1) + '\'s destination :<input id="p' + i + '" type="text" />';
+                conDiv.innerHTML += p;
+            }
+        }
+        submitButton.disabled = false;
+    };
 
-	submitButton.onclick = function () {
-		stops = Number(document.getElementById('stops').value);
+    submitButton.onclick = function () {
+        stops = Number(document.getElementById('stops').value);
 
-		if (isNaN(stops) || stops < 1) {
-			result.innerHTML = '<span style="text:red;">The Lift is an elevator right??? It has to stop somewhere!!! Enter a number >0</span>';
-		} else if (isNaN(pass) || pass < 1) {
-			result.innerHTML = 'Zero Passengers!!! Enter a number > 0';
-			submitButton.disabled = true;
-			return;
-		} else {
-			var dfloors = new Array();
-			for (var i = 0; i < pass; i++) {
-				var p = document.getElementById('p' + i);
-				if (p.value && !(isNaN(p.value)) && p.value > 0) {
-					dfloors.push(p.value);
-				} else {
-					result.innerHTML = 'Please enter Destination for passenger :' + (i + 1);
-					return;
-				}
-			}
-			console.log(dfloors.toString());
-			dfloors.sort(function (a, b) {
-				return a - b
-			});
-			console.log(dfloors.toString());
-			if (stops == 1) {
-				result.innerHTML = 'Optimized floor to stop: ' + calcAvg(dfloors) + ' and minimum walk required : ' + calcDeviationForArray(dfloors);
-			} else if (pass <= stops) {
-				if (dfloors.length != pass) {
-					result.innerHTML = 'No. of passengers not equal to no. of destination floors';
-				} else {
-					result.innerHTML = 'Floors to stop: ' + dfloors.toString();
-				}
-			} else if (dfloors.length != pass) {
-				result.innerHTML = 'No. of passengers not equal to no. of destination floors';
-			} else {
-				var optimizedResult = evaluate(pass - 1, stops - 1, dfloors);
-				result.innerHTML = optimizedResult;
-			}
-		}
-	}
-}
+        if (isNaN(stops) || stops < 1) {
+            result.innerHTML = '<span style="text:red;">The Lift is an elevator right??? It has to stop somewhere!!! Enter a number >0</span>';
+        } else if (isNaN(pass) || pass < 1) {
+            result.innerHTML      = 'Zero Passengers!!! Enter a number > 0';
+            submitButton.disabled = true;
+            return;
+        } else {
+            var dfloors = new Array();
+            for (var i = 0; i < pass; i++) {
+                var p = document.getElementById('p' + i);
+                if (p.value && !(isNaN(p.value)) && p.value > 0) {
+                    dfloors.push(p.value);
+                } else {
+                    result.innerHTML = 'Please enter Destination for passenger :' + (i + 1);
+                    return;
+                }
+            }
+            console.log(dfloors.toString());
+            dfloors.sort(function (a, b) {
+                return a - b;
+            });
+            console.log(dfloors.toString());
+            if (stops == 1) {
+                result.innerHTML = 'Optimized floor to stop: ' + calcAvg(dfloors) + ' and minimum walk required : ' + calcDeviationForArray(dfloors);
+            } else if (pass <= stops) {
+                if (dfloors.length != pass) {
+                    result.innerHTML = 'No. of passengers not equal to no. of destination floors';
+                } else {
+                    result.innerHTML = 'Floors to stop: ' + dfloors.toString();
+                }
+            } else if (dfloors.length != pass) {
+                result.innerHTML = 'No. of passengers not equal to no. of destination floors';
+            } else {
+                var optimizedResult = evaluate(pass - 1, stops - 1, dfloors);
+                result.innerHTML    = optimizedResult;
+            }
+        }
+    };
+};
+
 function evaluate(pass, stops, dfloors) {
-	var iterations,
-	maxFloor = dfloors[dfloors.length - 1];
-	iterations = getnCrList(pass, stops);
-	var resultIndex = 0;
-	for (var i = 0; i < iterations.length; i++) {
-		var indexElem = iterations[i];
-		var groups = splitIntoGroups(dfloors, indexElem);
-		console.log(indexElem.toString());
-		iterations[i].stopFloorArr = calcGroupsAvges(groups);
-		iterations[i].amntToWalk = calcDeviationForGroups(groups);
-		console.log(iterations[i].stopFloorArr);
-		console.log(iterations[i].amntToWalk);
-		if (iterations[i].amntToWalk < iterations[resultIndex].amntToWalk)
-			resultIndex = i;
-	}
-	return 'Optimized floors to stop: ' + iterations[resultIndex].stopFloorArr.toString() + ' and minimum walk required : ' + iterations[resultIndex].amntToWalk;
+    var iterations,
+        maxFloor    = dfloors[dfloors.length - 1];
+    iterations      = getnCrList(pass, stops);
+    var resultIndex = 0;
+    for (var i = 0; i < iterations.length; i++) {
+        var indexElem = iterations[i];
+        var groups    = splitIntoGroups(dfloors, indexElem);
+        console.log(indexElem.toString());
+        iterations[i].stopFloorArr = calcGroupsAvges(groups);
+        iterations[i].amntToWalk   = calcDeviationForGroups(groups);
+        console.log(iterations[i].stopFloorArr);
+        console.log(iterations[i].amntToWalk);
+        if (iterations[i].amntToWalk < iterations[resultIndex].amntToWalk)
+            resultIndex = i;
+    }
+    return 'Optimized floors to stop: ' + iterations[resultIndex].stopFloorArr.toString() + ' and minimum walk required : ' + iterations[resultIndex].amntToWalk;
 }
-function ncr(n, r) {
-	if (n < r)
-		return -1;
-	var result = 1,
-	p = n - r;
-	var i = 0;
-	while (n > r) {
-		result *= n--;
-		i++
-	}
-	while (p > 0) {
-		result /= p--;
-		i++
-	}
-	return result;
-}
-function getnCrList(n, r) {
-	var item = new Array(r);
-	var iterations = new Array();
-	for (var i = 0; i < item.length; i++) {
-		item[i] = i + 1;
-	}
 
-	var done = false;
-	while (!done) {
-		iterations.push(item.slice(0));
-		done = getNextItem(item, n, r);
-	}
-	return iterations;
+function ncr(n, r) {
+    if (n < r)
+        return -1;
+    var result = 1,
+        p      = n - r;
+    var i      = 0;
+    while (n > r) {
+        result *= n--;
+        i++;
+    }
+    while (p > 0) {
+        result /= p--;
+        i++;
+    }
+    return result;
+}
+
+function getnCrList(n, r) {
+    var item       = new Array(r);
+    var iterations = new Array();
+    for (var i = 0; i < item.length; i++) {
+        item[i] = i + 1;
+    }
+
+    var done = false;
+    while (!done) {
+        iterations.push(item.slice(0));
+        done = getNextItem(item, n, r);
+    }
+    return iterations;
 }
 
 function getNextItem(item, n, r) {
-	var dest = r - 1;
-	item[dest]++;
-	if (item[dest] > ((n - (r - dest)) + 1)) {
-		while (item[dest] > ((n - (r - dest)))) {
-			dest--;
-			if (dest < 0) {
-				break;
-			}
-		}
-		if (dest < 0) {
-			return true;
-		}
-		item[dest]++;
-		for (var i = dest + 1; i < item.length; i++) {
-			item[i] = item[i - 1] + 1;
-		}
-	}
-	return false;
+    var dest = r - 1;
+    item[dest]++;
+    if (item[dest] > ((n - (r - dest)) + 1)) {
+        while (item[dest] > ((n - (r - dest)))) {
+            dest--;
+            if (dest < 0) {
+                break;
+            }
+        }
+        if (dest < 0) {
+            return true;
+        }
+        item[dest]++;
+        for (var i = dest + 1; i < item.length; i++) {
+            item[i] = item[i - 1] + 1;
+        }
+    }
+    return false;
 }
 
 function splitIntoGroups(dfloors, indexElem) {
-	var groups = new Array(),
-	start = 0,
-	l = indexElem.length,
-	end = indexElem[l - 1];
-	if (l) {
-		groups.push(dfloors.slice(start, indexElem[0]));
-		for (var i = 0; i < l - 1; i++) {
-			groups.push(dfloors.slice(indexElem[i], indexElem[i + 1]));
-		}
-		groups.push(dfloors.slice(indexElem[l - 1]));
-	}
-	return groups;
+    var groups = new Array(),
+        start  = 0,
+        l      = indexElem.length,
+        end    = indexElem[l - 1];
+    if (l) {
+        groups.push(dfloors.slice(start, indexElem[0]));
+        for (var i = 0; i < l - 1; i++) {
+            groups.push(dfloors.slice(indexElem[i], indexElem[i + 1]));
+        }
+        groups.push(dfloors.slice(indexElem[l - 1]));
+    }
+    return groups;
 }
+
 function calcGroupsAvges(groups) {
-	var avgArr = new Array();
-	for (var i = 0, l = groups.length; i < l; i++) {
-		avgArr.push(calcAvg(groups[i]));
-	}
-	return avgArr;
+    var avgArr = new Array();
+    for (var i = 0, l = groups.length; i < l; i++) {
+        avgArr.push(calcAvg(groups[i]));
+    }
+    return avgArr;
 }
+
 function calcAvg(array) {
-	var sum = 0,
-	i = array.length;
-	while (i--) {
-		sum += +array[i];
-	}
-	var avg = Math.floor(sum / array.length);
-	return avg;
+    var sum = 0,
+        i   = array.length;
+    while (i--) {
+        sum += +array[i];
+    }
+    var avg = Math.floor(sum / array.length);
+    return avg;
 }
+
 function calcDeviationForGroups(groups) {
-	var sum = 0;
-	for (var i = 0, l = groups.length; i < l; i++) {
-		sum += +calcDeviationForArray(groups[i]);
-	}
-	return sum;
+    var sum = 0;
+    for (var i = 0, l = groups.length; i < l; i++) {
+        sum += +calcDeviationForArray(groups[i]);
+    }
+    return sum;
 }
+
 function calcDeviationForArray(array) {
-	var avg = calcAvg(array),
-	sum = 0;
-	for (var j = 0; j < array.length; j++) {
-		sum +=  + (Math.abs(array[j] - avg));
-	}
-	return sum;
+    var avg = calcAvg(array),
+        sum = 0;
+    for (var j = 0; j < array.length; j++) {
+        sum += +(Math.abs(array[j] - avg));
+    }
+    return sum;
 }
--- MaxSubMatrix/msm.js
+++ MaxSubMatrix/msm.js
@@ -1,102 +1,105 @@
 window.onload = function () {
-	var submitMN = document.getElementById('submitMN'),
-	result = document.getElementById('result');
+    var submitMN = document.getElementById('submitMN'),
+        result   = document.getElementById('result');
 
-	submitMN.onclick = function () {
-		var matm = document.getElementById('rows').value,
-		matn = document.getElementById('columns').value,
-		matDiv = document.getElementById('MatDiv');
-		matDiv.innerHTML = '';
-		if (isNaN(matm) || matm < 1) {
-			result.innerHTML = 'Please enter a number for <b>"ROWS"</b>!!!';
-			return;
-		} else if (isNaN(matn) || matn < 1) {
-			result.innerHTML = 'Please enter a number for <b>"Columns"</b>!!!';
-			return;
-		}
-		for (var i = 0; i < matm; i++) {
-			if (i < 9)
-				matDiv.innerHTML += '<br />Row:&nbsp;&nbsp;&nbsp;&nbsp;' + (i + 1) + '&nbsp;&nbsp;&nbsp;&nbsp;';
-			else
-				matDiv.innerHTML += '<br />Row:&nbsp;&nbsp;&nbsp;' + (i + 1) + '&nbsp;&nbsp;&nbsp;';
-			for (var j = 0; j < matn; j++) {
-				var p = '<input id="m' + i + j + '" type="text" style="width: 1em;"/>';
-				matDiv.innerHTML += p;
-			}
-		}
-		matDiv.innerHTML += '<br /><input id="submitMat" type="submit" value="Calculate" onclick="calculate(' + matm + ',' + matn + ')"/>';
-	}
-}
+    submitMN.onclick = function () {
+        var matm         = document.getElementById('rows').value,
+            matn         = document.getElementById('columns').value,
+            matDiv       = document.getElementById('MatDiv');
+        matDiv.innerHTML = '';
+        if (isNaN(matm) || matm < 1) {
+            result.innerHTML = 'Please enter a number for <b>"ROWS"</b>!!!';
+            return;
+        } else if (isNaN(matn) || matn < 1) {
+            result.innerHTML = 'Please enter a number for <b>"Columns"</b>!!!';
+            return;
+        }
+        for (var i = 0; i < matm; i++) {
+            if (i < 9)
+                matDiv.innerHTML += '<br />Row:&nbsp;&nbsp;&nbsp;&nbsp;' + (i + 1) + '&nbsp;&nbsp;&nbsp;&nbsp;';
+            else
+                matDiv.innerHTML += '<br />Row:&nbsp;&nbsp;&nbsp;' + (i + 1) + '&nbsp;&nbsp;&nbsp;';
+            for (var j = 0; j < matn; j++) {
+                var p = '<input id="m' + i + j + '" type="text" style="width: 1em;"/>';
+                matDiv.innerHTML += p;
+            }
+        }
+        matDiv.innerHTML += '<br /><input id="submitMat" type="submit" value="Calculate" onclick="calculate(' + matm + ',' + matn + ')"/>';
+    };
+};
+
 function calculate(m, n) {
-	var mat = new Array(),
-	result = document.getElementById('result');
-	for (var i = 0; i < m; i++) {
-		var row = new Array();
-		for (var j = 0; j < n; j++) {
-			var elem = document.getElementById('m' + i + j).value;
-			if (!elem || isNaN(elem) || elem < 0 || elem > 1) {
-				result.innerHTML = 'Please enter "1" or "0" for element in Row :' + (i + 1) + ', Column : ' + (j + 1);
-				return;
-			} else {
-				result.innerHTML = '';
-				row.push(elem);
-			}
-		}
-		mat.push(row);
-	}
-	console.log('Matrix : ', mat.toString());
-	var len = calcDMSM(mat);
-	result.innerHTML = '<br />Max Sub Matrix Start: ' + (len[1] + 1) + ',' + (len[2] + 1) + '<br />Max Sub Matrix End: ' + (len[3] + 1) + ',' + (len[4] + 1) + '<br />Max Sub Matrix Length: ' + len[0];
+    var mat    = new Array(),
+        result = document.getElementById('result');
+    for (var i = 0; i < m; i++) {
+        var row = new Array();
+        for (var j = 0; j < n; j++) {
+            var elem = document.getElementById('m' + i + j).value;
+            if (!elem || isNaN(elem) || elem < 0 || elem > 1) {
+                result.innerHTML = 'Please enter "1" or "0" for element in Row :' + (i + 1) + ', Column : ' + (j + 1);
+                return;
+            } else {
+                result.innerHTML = '';
+                row.push(elem);
+            }
+        }
+        mat.push(row);
+    }
+    console.log('Matrix : ', mat.toString());
+    var len          = calcDMSM(mat);
+    result.innerHTML = '<br />Max Sub Matrix Start: ' + (len[1] + 1) + ',' + (len[2] + 1) + '<br />Max Sub Matrix End: ' + (len[3] + 1) + ',' + (len[4] + 1) + '<br />Max Sub Matrix Length: ' + len[0];
 }
+
 function calcMSM(arr) {
-	if (arr) {
-		var rows = arr.length,
-		cols = arr[0].length,
-		len = new Array(1);
-		len[0] = 0;
-		for (var i = 0; i < rows; i++) {
-			for (var j = 0; j < cols; j++) {
-				var temp = calcD(i, j, rows, cols, arr);
-				console.log('temp : ', temp.toString());
-				if (temp[0] > len[0]) {
-					console.log('Got the MAX!!!!!!');
-					len = temp;
-				}
-			}
-		}
-		console.log('Returning : ', len.toString());
-		return len;
-	}
-	return;
+    if (arr) {
+        var rows = arr.length,
+            cols = arr[0].length,
+            len  = new Array(1);
+        len[0]   = 0;
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                var temp = calcD(i, j, rows, cols, arr);
+                console.log('temp : ', temp.toString());
+                if (temp[0] > len[0]) {
+                    console.log('Got the MAX!!!!!!');
+                    len = temp;
+                }
+            }
+        }
+        console.log('Returning : ', len.toString());
+        return len;
+    }
+    return;
 }
+
 function calc(si, sj, r, c, arr) {
-	var di = r,
-	dj = c,
-	len = new Array(5);
-	len[0] = 0;
-	len[1] = si;
-	len[2] = sj;
-	console.log('-----------------');
-	console.log('processing :', (si + 1), (sj + 1));
-	for (var i = si; i < di; i++) {
-		for (var j = sj; j < dj; j++) {
-			if (arr[i][j] == 1) {
-				var temp = (j - sj + 1) * (i - si + 1);
-				console.log('checking :', (i + 1), (j + 1), temp);
-				if (temp > len[0]) {
-					len[0] = temp;
-					len[3] = i;
-					len[4] = j;
-					console.log('len increased : ', (i + 1), (j + 1));
-				}
-			} else {
-				dj = j;
-				break;
-			}
-		}
-		if (dj == sj)
-			break;
-	}
-	console.log(len.toString());
-	return len;
+    var di  = r,
+        dj  = c,
+        len = new Array(5);
+    len[0]  = 0;
+    len[1]  = si;
+    len[2]  = sj;
+    console.log('-----------------');
+    console.log('processing :', (si + 1), (sj + 1));
+    for (var i = si; i < di; i++) {
+        for (var j = sj; j < dj; j++) {
+            if (arr[i][j] == 1) {
+                var temp = (j - sj + 1) * (i - si + 1);
+                console.log('checking :', (i + 1), (j + 1), temp);
+                if (temp > len[0]) {
+                    len[0] = temp;
+                    len[3] = i;
+                    len[4] = j;
+                    console.log('len increased : ', (i + 1), (j + 1));
+                }
+            } else {
+                dj = j;
+                break;
+            }
+        }
+        if (dj == sj)
+            break;
+    }
+    console.log(len.toString());
+    return len;
 }
